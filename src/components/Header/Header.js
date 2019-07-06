61e6d8353415c22a00dea20aa757a3bad5d018f4
Cobbie Quintos <cobbiequintos@gmail.com>
2019-07-06 20:21:38 +0800

    init commit ui

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../../stylesheets/header.css'
+import piplup from '../../images/pika.png'
+
+const Header = props => {
+    return(
+        <div id="main">
+            <div id="flex-container-main">
+                <div id="logo-header-container"><div id="logo">
+                <img 
+                src={piplup} 
+                alt="Logo"
+                id="logo"
+                />
+                </div>
+                <div id="pokedex">Pokedex</div>
+                </div>
+                <div id="tree-item-flex-container"><div className="tree-navbar">Tree Generation</div><div className="tree-navbar">Tree Traversal</div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
